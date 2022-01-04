f89a183879e9a08a7e181e4010d1e528cd021c31
Augustin BEGUE <augustin.begue@epita.fr>
2022-01-04 21:29:58 +0100

    alacritty: fixed colors

--- common/colors.nix
+++ common/colors.nix
@@ -1,11 +1,12 @@
 {
   background = "#282C34";
   background-alt = "#434954";
-  foreground = "#ffffff";
+  foreground = "#ABB2BF";
   foreground-alt = "#dfdfdf";
   primary = "#61AFEF";
   secondary = "#56B6C2";
-  alert = " #E06C75";
+  alert = "#E06C75";
   warning = "#E5C07B";
   success = "#98C379";
+  magenta = "#C678DD";
 }
--- home.nix
+++ home.nix
@@ -7,7 +7,7 @@ let
   alt = "Mod1";
   font = "CaskaydiaCove Nerd Font";
   terminal = "alacritty";
-  colors = import ./common/colors.nix;
+  _colors = import ./common/colors.nix;
 in
 {
   home.username = "abegue";
@@ -52,7 +52,62 @@ in
   };
 
   programs = {
-    
+    zsh = {
+      enable = true;
+
+      autocd = true;
+
+      shellAliases = {
+        ls = "ls -la";
+      };
+
+      initExtra = ''
+        # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      '';
+
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.7.0";
+            sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+          };
+          file = "zsh-autosuggestions.zsh";
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.6.0";
+            sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
+          };
+          file = "zsh-syntax-highlighting.zsh";
+        }
+        {
+          name = "enhancd";
+          src = pkgs.fetchFromGitHub {
+            owner = "b4b4r07";
+            repo = "enhancd";
+            rev = "v2.2.4";
+            sha256 = "1smskx9vkx78yhwspjq2c5r5swh9fc5xxa40ib4753f00wk4dwpp";
+          };
+          file = "init.sh";
+        }
+      ];
+
+      oh-my-zsh = {
+        enable = true;
+
+        plugins = [
+          "git"
+          "sudo"
+        ];
+      };
+    };
     alacritty = {
       enable = true;
       settings = {
@@ -61,45 +116,44 @@ in
             family = font;
           };
           size = 7;
-          # Colors (Tomorrow Night)
-          colors = {
-            # Default colors
-            primary = {
-              background = "0x282C34";
-              foreground = "0xabb2bf";
+        };
+        colors = {
+          # Default colors
+          primary = {
+            background = "${_colors.background}";
+            foreground = "${_colors.foreground}";
 
-              # Bright and dim foreground colors
-              #
-              # The dimmed foreground color is calculated automatically if it is not present.
-              # If the bright foreground color is not set, or `draw_bold_text_with_bright_colors`
-              # is `false`, the normal foreground color will be used.
-              #dim_foreground: "0x9a9a9a"
-              bright_foreground = "0xe6efff";
-            };
+            # Bright and dim foreground colors
+            #
+            # The dimmed foreground color is calculated automatically if it is not present.
+            # If the bright foreground color is not set, or `draw_bold_text_with_bright_colors`
+            # is `false`, the normal foreground color will be used.
+            #dim_foreground: "0x9a9a9a"
+            bright_foreground = "${_colors.foreground-alt}";
+          };
 
-            # Normal colors
-            normal = {
-              black = "0x1e2127";
-              red = "0xe06c75";
-              green = "0x98c379";
-              yellow = "0xd19a66";
-              blue = "0x61afef";
-              magenta = "0xc678dd";
-              cyan = "0x56b6c2";
-              white = "0x828791";
-            };
+          # Normal colors
+          normal = {
+            black = "${_colors.background}";
+            red = "${_colors.alert}";
+            green = "${_colors.success}";
+            yellow = "${_colors.warning}";
+            blue = "${_colors.primary}";
+            magenta = "${_colors.magenta}";
+            cyan = "${_colors.secondary}";
+            white = "${_colors.foreground}";
+          };
 
-            # Bright colors
-            bright = {
-              black = "0x5c6370";
-              red = "0xe06c75";
-              green = "0x98c379";
-              yellow = "0xd19a66";
-              blue = "0x61afef";
-              magenta = "0xc678dd";
-              cyan = "0x56b6c2";
-              white = "0xe6efff";
-            };
+          # Bright colors
+          bright = {
+            black = "${_colors.background}";
+            red = "${_colors.alert}";
+            green = "${_colors.success}";
+            yellow = "${_colors.warning}";
+            blue = "${_colors.primary}";
+            magenta = "${_colors.magenta}";
+            cyan = "${_colors.secondary}";
+            white = "${_colors.foreground}";
           };
         };
       };
