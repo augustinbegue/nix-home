04bd647dd4d0643497c041e97e85f20424c35d76
Augustin BEGUE <augustin.begue@epita.fr>
2022-01-05 10:29:02 +0100

    vim: tweaks

--- editors/vim.nix
+++ editors/vim.nix
@@ -29,8 +29,6 @@
     colorscheme onehalfdark
     let g:airline_powerline_fonts=1
     let g:airline_theme='onehalfdark'
-    " lightline
-    let g:lightline = { 'colorscheme': 'onehalfdark' }
     if exists('+termguicolors')
       let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
       let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
