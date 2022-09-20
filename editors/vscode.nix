2667c6116b0bffed67d2dedd84f48f0b9c097cc2
Augustin BEGUE <augustin.begue@epita.fr>
2022-09-20 08:59:10 +0000

    added extensions

--- editors/vscode.nix
+++ editors/vscode.nix
@@ -1,6 +1,25 @@
 { lib, pkgs, ... }:
 
-let vars = import ../common/vars.nix;
+let
+  vars = import ../common/vars.nix;
+  vscode-css-peek = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    publisher = "pranaygp";
+    name = "vscode-css-peek";
+    version = "4.2.0";
+    sha256 = "0dpkp3xs8jd826h2aa9xlfilsj4yv8q6r9cs350ljrpcyj7wrlpq";
+  };
+  html-css-class-completion = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "html-css-class-completion";
+    publisher = "Zignd";
+    version = "1.20.0";
+    sha256 = "1hc2dgib3wryygb36h47wzf32iv1x6rn1swmbgchiyjw62jjj4fw";
+  };
+  prisma = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "prisma";
+    publisher = "Prisma";
+    version = "4.3.1"; 
+    sha256 = "sha256-IccK7cdW2KjEKlgCC/K7zzs09VvS0fr2TBhR178VX1k";
+  };
 in
 {
   programs.vscode = {
@@ -15,6 +34,13 @@ in
         eamodio.gitlens
         github.copilot
         james-yu.latex-workshop
+        vscodevim.vim
+        vscode-css-peek
+        dbaeumer.vscode-eslint
+        html-css-class-completion
+        christian-kohler.path-intellisense
+        prisma
+        gruntfuggly.todo-tree 
       ];
       userSettings = {
         "workbench.colorTheme" = "One Dark Pro";
